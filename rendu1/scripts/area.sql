5f35e8a5215acd7b23e33cfd07f074bb4c91565d
Elise Klay <elise.klay@epfl.ch>
2014-03-10 22:53:39 +0100

    potato

---
+++ rendu1/scripts/area.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Areas
+( areaID CHAR(20), name CHAR(40), type CHAR(20),
+PRIMARY KEY (areaID) )
---
+++ rendu1/scripts/artist.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Artists
+( artistID CHAR(20), name CHAR(40), type CHAR(20), areaID CHAR(20), 
+PRIMARY KEY (artistID),
+FOREIGN KEY (areaID) REFERENCES Area )
