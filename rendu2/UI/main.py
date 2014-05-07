ed69068beb290bb9158cb610d6a5a5d29dbed74c
Ouikka <elise.klay@epfl.ch>
2014-05-07 21:09:17 +0200

    UI modification

--- rendu2/UI/main.py
+++ rendu2/UI/main.py
@@ -1,40 +1,51 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import*
 import mysql
-import os
 
 class MainWindow(QMainWindow):
 #    db = mysql.MySql()
     def __init__(self, parent = None):
         super(MainWindow,self).__init__(parent)
         self.setWindowTitle('La petite maison')
-        self.setMinimumSize(720,480)
-
-
         self.show()
-        w = QWidget()
+        self.w = QWidget()
         self.m = mysql.MySql()
-        self.m.setVisible(False)
-        self.m.resizeColumnsToContents()
-        self.m.setVisible(True)
-        self.l = QVBoxLayout(w)
-        self.setCentralWidget(w)
-        self.nextb = QPushButton("Next", clicked=self.m.nextb)
-        #self.nextb.clicked.connect(self.m.nextb)
-        self.l.addWidget(self.m)
-        self.l.addWidget(self.nextb)
+#        self.m = QWidget()
+        self.l = QVBoxLayout()
+        self.setCentralWidget(self.w)
+
+        self.h1 = QHBoxLayout()
+        self.queriesLayout = QVBoxLayout()
+        self.h1.addWidget(self.m,4)
         
+        self.queriesLayout.addWidget(QPushButton("Query A"))
+        self.queriesLayout.addWidget(QPushButton("Query B"))
+        self.queriesLayout.addWidget(QPushButton("Query C"))
+        self.queriesLayout.addWidget(QPushButton("Query D"))
+        self.queriesLayout.addWidget(QPushButton("Query E"))
+        self.queriesLayout.addWidget(QPushButton("Query F"))
+        self.queriesLayout.addWidget(QPushButton("Query G"))
+        self.queriesLayout.addWidget(QPushButton("Query H"))
+
+        self.h1.addLayout(self.queriesLayout,1)
+
+
+        self.h2 = QHBoxLayout() 
+
+        self.h2.addWidget(QPushButton("Prev"))
+        self.h2.addWidget(QPushButton("Next"))
+        self.l.addLayout(self.h1)
+        self.l.addLayout(self.h2)
 #        self.m.show()
-        #self.setLayout(self.l)
-        
+        self.w.setLayout(self.l)
+        self.setFixedSize(1024,768)
         
 
 from sys import argv,exit
 if __name__=='__main__':
-	#os.environ['ORACLE_HOME']="/home/tachikoma/app/tachikoma/product/12.1.0/dbhome_1"
-	app = QApplication(argv)
-	screen = MainWindow()
+    app = QApplication(argv)
+    screen = MainWindow()
     #MainWindow.show()
-	exit(app.exec_())
+    exit(app.exec_())
--- rendu2/UI/mysql.py
+++ rendu2/UI/mysql.py
@@ -1,3 +1,4 @@
+
 from PyQt5.QtSql import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *#QAbstractTableModel
@@ -15,24 +16,30 @@ class TableModel(QAbstractTableModel):
     def data(self,index,role=Qt.DisplayRole):
         return self.table[index.row()][index.column()]
 
-
 class MySql(QTableView):
 
-	def __init__(self,host='icoracle.epfl.ch',port=1521,usr='db2014_g24',pw='db2014_g24',parent=None):
-		super(QTableView,self).__init__(parent)
-		self.engine = create_engine("oracle://db2014_g24:db2014_g24@icoracle.epfl.ch/srso4")
-		self.connection = self.engine.connect()
-		self.meta = MetaData(bind = self.engine, reflect=True)
-		releases = self.meta.tables['releases']
-		self.results = self.connection.execute(select([releases]))
-		self.table = TableModel(self.results.fetchmany(50))
-		self.setModel(self.table)
-		self.horizontalHeader().setStretchLastSection(True)
-        
-	def __del__(self):
-		self.connection.close()
-
-	def nextb(self):	
-		self.table = TableModel(self.results.fetchmany(50))
-		self.setModel(self.table)
-
+    def __init__(self,host='icoracle.epfl.ch',port=1521,usr='db2014_g24',pw='db2014_g24',parent=None):
+        super(QTableView,self).__init__(parent)
+        self.engine = create_engine("oracle://db2014_g24:db2014_g24@icoracle.epfl.ch/srso4")
+        self.connection = self.engine.connect()
+        self.meta = MetaData(bind = self.engine, reflect=True)
+        releases = self.meta.tables['releases']
+        results = self.connection.execute(select([releases]))
+        t = results.fetchmany(50)
+        #print(t)
+        self.table = TableModel(t)
+        self.setModel(self.table)
+        self.horizontalHeader().setStretchLastSection(True)
+        self.resizeColumnsToContents()
+	self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.customMenuRequested)
+ 
+    @pyqtSlot(QPoint)
+    def customMenuRequested(self, pos):
+        self.menu = QMenu()
+        self.menu.addAction(QAction("Artist",self))
+        self.menu.addAction(QAction("Release",self))
+        self.menu.popup(self.viewport().mapToGlobal(pos))
+   
+    def __del__(self):
+        self.connection.close()
