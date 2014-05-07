c534a37799ab5cb396135bf41e752778ac2b3eba
Bvinhmau <baptiste.vinhmau@epfl.ch>
2014-05-07 14:02:37 +0200

    Update F.sql

--- rendu2/scripts/F.sql
+++ rendu2/scripts/F.sql
@@ -1,12 +1,12 @@
-SELECT 	AreaId 
+SELECT 	 areamalefemale.topname
 FROM (	
-	SELECT 		city.areaid, 
+	SELECT 		city."NAME" topname , city.areaid, 
 				count(CASE WHEN arti.gender = 'Female' THEN 1 END) AS females, 
 				count(CASE WHEN arti.gender = 'Male' THEN 1 END) AS males  
 	FROM 		Artists arti  
 	INNER JOIN	Areas city 
 	ON 			city.areaid = arti.areaId   
 	WHERE 		city.type = 'City' 
-	GROUP BY 		city.areaId 
+	GROUP BY 		city.areaId , city."NAME"
 ) areamalefemale 
 WHERE 	areamalefemale.Females > areamalefemale.Males
