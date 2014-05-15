a9a4d46675d0d7dc4032ba25efb23d03aca85621
Bvinhmau <baptiste.vinhmau@epfl.ch>
2014-05-15 17:53:08 +0200

    Update J.sql
    Reste a selectionner artiste avec le + de tracks par genre

--- rendu3/scripts/J.sql
+++ rendu3/scripts/J.sql
@@ -1,8 +1,27 @@
 -- For each of the 10 genres with the most artist, list the female artist that has recorder the highest number of tracks
--- Retourne les genres ordonnés par nombre d'artistes
-
-Select GenreId, count(*) counter from Artist_GENRE GROUP BY GenreId ORDER BY counter DESC 
-
-
--- Pour un genre fixé, retourne l'artiste féminin avec le plus de track
-Select * from (Select artilist.ArtistId, artilist."NAME", artilist.AreaId, artilist.gender, artilist."TYPE" , count(*) counter from Track_Artist INNER JOIN (Select Artists.* from Artists INNER JOIN (Select * from Artist_Genre where GenreId = 5) artigenre ON artigenre.artistId = Artists.ArtistId where Artists.gender = 'Female') artilist ON Track_Artist.artistid = artilist.artistid GROUP BY  artilist.ArtistId, artilist."NAME", artilist.AreaId, artilist.gender, artilist."TYPE" ORDER BY counter DESC) WHERE ROWNUM <=1 
+	Select artilist.ArtistId, artilist."NAME", artilist.AreaId, artilist.gender, artilist."TYPE" , count(*) counter , genreId
+	from Track_Artist 
+	INNER JOIN ( 
+	      Select Artists.*  , genreid
+	      from Artists 
+	      INNER JOIN (
+			Select ArtistId, genreids.genreid 
+			from Artist_Genre 
+			INNER JOIN (
+				  Select GenreId 
+				  from (
+					Select *
+					From (
+						Select GenreId, count(*) counter 
+						from Artist_GENRE 
+						GROUP BY GenreId 
+						ORDER BY counter DESC  )
+					WHERE 	ROWNUM <=4)
+			) genreids
+			ON genreids.genreid = Artist_Genre.genreid
+		      ) artigenre 
+	      ON artigenre.artistId = Artists.ArtistId 
+	      where Artists.gender = 'Female' ) artilist 
+	ON Track_Artist.artistid = artilist.artistid 
+	GROUP BY  artilist.ArtistId, artilist."NAME", artilist.AreaId, artilist.gender, artilist."TYPE" , genreid
+	ORDER BY genreid, counter DESC 
