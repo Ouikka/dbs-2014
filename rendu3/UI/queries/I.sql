27b77e0a353944db79a3c53f4fd27fa513de9998
Ouikka <elise.klay@epfl.ch>
2014-06-04 01:08:52 +0200

    I

--- rendu3/UI/queries/I.sql
+++ rendu3/UI/queries/I.sql
@@ -1,3 +1,7 @@
-SELECT a.artistID metallicaid, a.name
-FROM Artists a, Areas l
-WHERE name='Metallica' AND a.areaID=l.areaID AND l.name='United States' 
+SELECT R.name
+FROM Track_artist Ta,Tracks T,Artists A, Recordings R
+WHERE a.name="Metallica" and a.artistID=Ta.artistID and T.trackID=Ta.trackID and T.recordingId=R.recordingId
+GROUP BY T.recordingID
+ORDER BY COUNT(*) DESC
+LIMIT 25
+
--- rendu3/scripts/I.sql
+++ rendu3/scripts/I.sql
@@ -1,10 +1,7 @@
 SELECT R.name
-FROM Recordings R 
-INNER JOIN (SELECT T.recordingID as record,COUNT(*) as c
-			FROM Track_artist Ta,Tracks T,Artists A
-			WHERE a.name="Metallica" and a.artistID=Ta.artistID and T.trackID=Ta.trackID
-			GROUP BY T.recordingID
-			ORDER BY c DESC
-			LIMIT 25)
-ON R.recordingID=record
+FROM Track_artist Ta,Tracks T,Artists A, Recordings R
+WHERE a.name="Metallica" and a.artistID=Ta.artistID and T.trackID=Ta.trackID and T.recordingId=R.recordingId
+GROUP BY T.recordingID
+ORDER BY COUNT(*) DESC
+LIMIT 25
  
